0f2d13460c227f63148fc9c78d710533e56e680a
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-20 21:34:49 -0300

    navbar and products page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
-import { styled } from './../styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 'none',
-  padding: '4px 8px',
-
-  span: {
-    color: 'white',
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import shirt1 from './../assets/camisetas/1.png'
+import shirt2 from './../assets/camisetas/2.png'
+// import shirt3 from './../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      <span>Enviar</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      {/* <Product>
+        <Image src={shirt3} width={520} height={480} alt='' />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,21 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6'
+      white: '#fff',
+
+      gray100: '#e1e1e6',
+      gray300: '#c4c4cc',
+      gray800: '#202024',
+      gray900: '#121214',
+
+      green300: '#00b37e',
+      green500: '#00875f',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
     }
-  }
+  },
 })
