a0b079472721581eb97aa410c4d64fb6874350ed
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-07-01 20:02:19 -0300

    success page and seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Stripe from 'stripe'
 import { stripe } from '@/lib/stripe'
 
 import { HomeContainer, Product } from '@/styles/pages/home'
+import Head from 'next/head'
 
 interface HomeProps {
   products: {
@@ -28,24 +29,30 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map((product) => {
-        return (
-          <Product
-            href={`/product/${product.id}`}
-            className='keen-slider__slide'
-            key={product.id}
-            prefetch={false}
-          >
-            <Image src={product.imageUrl} width={520} height={480} alt='' />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-        </Product>
-        )
-      })}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map((product) => {
+          return (
+            <Product
+              href={`/product/${product.id}`}
+              className='keen-slider__slide'
+              key={product.id}
+              prefetch={false}
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt='' />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+          </Product>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
